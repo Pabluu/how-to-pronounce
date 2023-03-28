ecc102e15f6e79f90653e9484457d11b6764bbff
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-28 16:31:24 -0300

    change: placeholder

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Input = ({ ...params }) => {
-  return <input type="text" placeholder="Termo a procurar" {...params} />;
+  return <input type="text" placeholder="Digite o termo" {...params} />;
 };
 
 export default Input;

83eafe376783c0b6d1bd2c6b086cc3b6e89ebc0f
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-28 16:21:36 -0300

    initial: pagina inicial finalizada

--- src/App.js
+++ src/App.js
@@ -4,17 +4,39 @@ import Button from "./Button";
 import { useEffect, useState } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    document.title = "How To Pronounce";
+  });
+
+  // useEffect(() => {
+
+  // }, [input])
+
+  function removeSpace({ target }) {
+    setInput(target.value.replace(" ", ""));
+  }
+
+  function searchPhonetic() {
+    setInput("");
+  }
   return (
     <>
-      <header class="title">How To Pronounce</header>
-      <Input />
-      <Button />
-
-      <div className="box-search">
+      <header className="title">How To Pronounce</header>
+      <main className="container">
+        <div className="box-search">
         <Label texto="Resultado" class="title" />
         <Label texto="In" class="busca" />
         <Label texto="/in/" class="resultado" />
       </div>
+
+        {/* <div className="box-found">
+          <Label texto="Resultado" className="title" />
+          <Label texto="In" className="busca" />
+          <Label texto="/in/" className="resultado" />
+        </div> */}
+      </main>
     </>
   );
 }

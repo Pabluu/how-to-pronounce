f5a150b54b49566077cabc815da52de3a18033ae
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-29 16:12:25 -0300

    trabalhando com o cursor

--- src/Label.js
+++ src/Label.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Label = ({ texto, ...args }) => {
-  return <label {...args}>{texto}</label>;
+const Label = ({ texto, cursor, ...args }) => {
+  return (
+    <label style={{ cursor: cursor }} {...args}>
+      {texto}
+    </label>
+  );
 };
 
 export default Label;

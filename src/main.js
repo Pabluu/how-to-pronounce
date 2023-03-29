d687dbab71b84c3ff795c00f1842fe788634f438
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-29 00:09:10 -0300

    arquivo responsável por pesquisar o termo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
-fetch('https://api.dictionaryapi.dev/api/v2/entries/en/in').then(
-  resp => resp.json() // this returns a promise
-).then(repos => {
-  console.log(repos[0]['phonetics']['text'])
-}).catch(ex => {
-  console.error(ex);
-})
+// Função responsável por buscar pelo termo
+export async function searchPhonetic(term) {
+  const query = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${term}`
+  );
+  const response = await query.json();
+  const data = response[0]["phonetics"];
+  const positionAudio = checkAudio(data);
+
+  return data[positionAudio];
+}
+
+// Função responsável por saber qual id do objeto possui um audio
+function checkAudio(data) {
+  let index = 0;
+
+  for (let indexItem in data) {
+    if (data[indexItem].audio) {
+      return indexItem;
+    }
+  }
+  return index;
+}

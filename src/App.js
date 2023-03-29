b39c4c3b1ae81effec51dd5f4ddbbce8fd0d7f7d
Pablo Henrique SC <pabloph0077@gmail.com>
2023-03-29 16:20:34 -0300

    fix bug: validação do input e alterando o cursor

--- src/App.js
+++ src/App.js
@@ -1,42 +1,89 @@
 import Input from "./Input";
 import Button from "./Button";
-// import Label from "./Label";
-import { useEffect, useState } from "react";
+import Label from "./Label";
+import { useState } from "react";
 
+import { searchPhonetic } from "./main";
+import { playAudio, stopAudio } from "./Sound";
+
+// Função principal do projeto
 export default function App() {
   const [input, setInput] = useState("");
+  const [phoneticText, setPhoneticText] = useState("");
+  const [audio, setAudio] = useState("");
+  const [text, setText] = useState("");
 
-  useEffect(() => {
-    document.title = "How To Pronounce";
-  });
-
-  // useEffect(() => {
-
-  // }, [input])
-
+  // Remove o espaço em branco do input
   function removeSpace({ target }) {
     setInput(target.value.replace(" ", ""));
   }
 
-  function searchPhonetic() {
-    setInput("");
+  // Faz a consulta ao pressionar o ENTER
+  function enterSearch({ code, target }) {
+    if (code === "Enter") {
+      if (isValidate()) {
+        search(target.value);
+      }
+    }
   }
+
+  // Função que verifica se o input possui algum texto
+  function isValidate() {
+    if (input) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Consulta o termo que o usuario escreveu
+  async function search() {
+    if (isValidate()) {
+      let consulta = await searchPhonetic(input);
+      setAudio(consulta.audio);
+      setPhoneticText(consulta.text);
+      setText(input);
+    }
+  }
+
   return (
     <>
       <header className="title">How To Pronounce</header>
+      {/* HEADER */}
+
       <main className="container">
         <div className="box-search">
-        <Label texto="Resultado" class="title" />
-        <Label texto="In" class="busca" />
-        <Label texto="/in/" class="resultado" />
-      </div>
+          <Input
+            value={input}
+            onChange={removeSpace}
+            onKeyPress={enterSearch}
+          />
+          <Button onClick={search} texto={"Procurar"} />
+        </div>
 
-        {/* <div className="box-found">
-          <Label texto="Resultado" className="title" />
-          <Label texto="In" className="busca" />
-          <Label texto="/in/" className="resultado" />
-        </div> */}
+        <div className="box-found">
+          <Label texto={text} className="busca" />
+
+          {phoneticText && (
+            <>
+              <audio controls src={audio} type="audio/mpeg" id="audio" />
+              <Label
+                texto={phoneticText}
+                className="resultado"
+                onMouseOver={() => {
+                  playAudio(audio);
+                }}
+                cursor={!audio && "not-allowed"}
+                title={!audio ? "Sem Audio" : ""}
+                onMouseOut={() => {
+                  stopAudio(audio);
+                }}
+              />
+            </>
+          )}
+        </div>
       </main>
+      {/* MAIN */}
     </>
   );
 }
